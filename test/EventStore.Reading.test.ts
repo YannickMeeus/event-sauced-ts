f255365e83a117329c8ad38a37b69715c697ee9f
Yannick Meeus <yannick.meeus@zalando.fi>
2018-06-26 10:53:41 +0300

    Tests - Event Store Reading - Read full stream

--- test/EventStore.Reading.test.ts
+++ test/EventStore.Reading.test.ts
@@ -2,6 +2,9 @@ import { InMemoryStorageEngine } from '../src/inMemory/InMemoryStorageEngine'
 import { IStorageEngine } from '../src/IStorageEngine'
 import * as uuid from 'uuid'
 import { EventStore } from '../src/EventStore'
+import { EventData } from '../src/EventData'
+import { OrderCreated } from './Events/OrderCreated'
+import { OrderDispatched } from './Events/OrderDispatched'
 
 describe('Given a set of engines to test against', () => {
   const engineFactories: (() => IStorageEngine)[] = [() => new InMemoryStorageEngine()]
@@ -20,6 +23,23 @@ describe('Given a set of engines to test against', () => {
         const events = await sut.readStreamForwards(streamId)
 
         expect(events.length).toEqual(0)
+      })
+    })
+    describe('When reading a stream with multiple events', () => {
+      it('It should return all events', async () => {
+        const streamId = newGuid()
+        const sut = await getStore()
+        const firstEvent = new EventData(newGuid(), new OrderCreated(streamId))
+        const secondEvent = new EventData(newGuid(), new OrderDispatched(streamId))
+
+        await sut.AppendToStream(streamId, 0, firstEvent)
+        await sut.AppendToStream(streamId, 1, secondEvent)
+
+        const stream = await sut.readStreamForwards(streamId)
+
+        expect(stream.length).toEqual(2)
+        expect(stream.shift()!.eventBody).toBeInstanceOf(OrderCreated)
+        expect(stream.shift()!.eventBody).toBeInstanceOf(OrderDispatched)
       })
     })
   })
