21ee4d1815221ea664213a35348af408ad445550
Yannick Meeus <yannick.meeus@zalando.fi>
2018-11-22 11:12:21 +0200

    docs(EventData.ts):

--- src/EventData.ts
+++ src/EventData.ts
@@ -1,6 +1,23 @@
 import { Guard } from './errors/Guard'
 
+/**
+ * This class is your primary interaction data model with the EventStor itself.
+ *
+ * @class EventData
+ */
 class EventData {
+  /**
+   * Creates an instance of EventData.
+   * @param {string} eventId The unique identifier for this event.
+   * This should be truly unique (any UUID/GUID will usually do)
+   * @param {*} body The event itself, this represents your domain
+   * event that needs persisting
+   * @param {*} [metaData=undefined] Optional. If you want to specify any
+   * meta-data that is not related to the intention of the event, but rather
+   * any other runtime information. Examples could include which server persisted
+   * the event, or the time it was persisted.
+   * @memberof EventData
+   */
   constructor(
     public readonly eventId: string,
     public readonly body: any,
