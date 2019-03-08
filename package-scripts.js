d4ab46197902d56454ec2d73a09c2cb3dc305049
Yannick Meeus <yannick.meeus@gmail.com>
2019-03-08 15:13:18 +0200

    chore(nps): Move publishing dryrun script to nps

--- package-scripts.js
+++ package-scripts.js
@@ -17,8 +17,9 @@ module.exports = {
       generate: 'typedoc --out docs --target es6 --theme minimal --mode file src',
       deploy: 'ts-node tools/gh-pages-publish'
     },
-    deploy: {
+    public: {
       default: 'semantic-release',
+      dryrun: 'node-env-run -E ./secrets/publishing.env --exec \'semantic-release --dry-run',
       prepare: 'ts-node tools/semantic-release-prepare.ts'
     },
     coverage: {
